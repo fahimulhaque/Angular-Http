b67c2db9cddab9cc62b180c7a8d265d2f4a49030
fahimulhaque <ffahimulhaque9@gamil.com>
2018-08-30 22:32:18 +0530

    added navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,46 @@
 import { AppErrorHandler } from './common/app-error-handler';
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule, ErrorHandler } from '@angular/core';
+import { NgModule, ErrorHandler, Component } from '@angular/core';
 import { HttpModule , } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { PostsComponent } from './posts/posts.component';
+import { GithubFollowersComponent } from './github-followers/github-followers.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RouterModule , Routes } from '@angular/router';
+import { NotfoundComponent } from './notfound/notfound.component';
+
+const routes: Routes = [
+  { path: '',
+    redirectTo: '/followers',
+    pathMatch: 'full'
+  },
+  { path: 'followers',
+    component: GithubFollowersComponent
+  },
+  { path: 'posts',
+    component: PostsComponent
+  },
+  { path: '**',
+    component: NotfoundComponent
+  }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostsComponent
+    PostsComponent,
+    GithubFollowersComponent,
+    NavbarComponent,
+    NotfoundComponent
   ],
   imports: [
     BrowserModule,
-    HttpModule
-
+    HttpModule,
+    RouterModule.forRoot(routes)
   ],
+  exports: [ RouterModule ],
+
   providers: [
     {provide: ErrorHandler , useClass: AppErrorHandler },
       {provide: String, useValue: 'stringValue'}
